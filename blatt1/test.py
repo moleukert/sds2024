051e0489246319bd5f20e05f2d9657343e0e7627
moleukert <132389463+moleukert@users.noreply.github.com>
2024-05-05 00:29:04 +0200

    fixed error with pearson corr.

--- blatt1/test.py
+++ blatt1/test.py
@@ -6,7 +6,7 @@ def test():
     x = np.asarray([1,2,3,4,5],dtype=int)
     y = np.asarray([9.5,21.86,30.65,45.52,49.77],dtype=float)
     # co-variance
-    s_xy = 1/(n-1) * np.sum((x-np.mean(x))*(y-np.mean(y)))
+    s_xy = 1/(n) * np.sum((x-np.mean(x))*(y-np.mean(y)))
     # standard deviations
     s_x = np.sqrt(1/n* np.sum(np.square(x-np.mean(x))))
     s_y = np.sqrt(1/n* np.sum(np.square(y-np.mean(y))))
@@ -19,6 +19,8 @@ def test():
     d = np.mean(y)- k * np.mean(x)
     y_3 = k*x[2]+d
     # f(3)
+    print(k)
+    print(d)
     print(y_3)
 
 if __name__ == "__main__":
